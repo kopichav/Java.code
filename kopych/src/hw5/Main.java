2a2b498b85ddf82103c8bd4c2a3771b1b4046331
kopichav <kopich@i.ua>
2022-12-29 12:32:02 +0200

    Create Students in hw5

--- kopych/src/hw5/Main.java
+++ kopych/src/hw5/Main.java
@@ -4,4 +4,7 @@ public class Main {
     public static void main(String[] args, Object Student1){
      Student1 student1 = new student1();
      Student1 student2 = new student1();
+     Student1 student3 = new student1();
+     student1 student4 = new student1();
+     student1 student5 = new student1();
     }
